8c70b227d0209df5c1f06199e363c8ef22d16955
Andrea Tassi <andreatassi98@gmail.com>
2024-06-25 03:23:28 +0200

    NavMesh and Agent

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [Range(0.0f, 30.0f)] public float movementSpeed = 20f;
+    [Range(0.0f, 30.0f)] public float movementSpeed = 10f;
     [Range(0.0f, 30.0f)] public float zoomSpeed = 10f;
+
+    public Vector3 min = new Vector3(-16f, 6f, -20f);
+    public Vector3 max = new Vector3(16f, 30f, 6f);
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,11 @@ public class CameraController : MonoBehaviour
     {
         Vector3 movement = Move();
         movement += Zoom();
-        transform.Translate(movement * Time.deltaTime);
+        Vector3 newPosition = transform.position + movement * Time.deltaTime;
+
+        newPosition = Clamp(newPosition);
+
+        transform.position = newPosition;
     }
 
     private Vector3 Move()
@@ -32,5 +39,14 @@ public class CameraController : MonoBehaviour
         float zoomInput = Input.GetAxisRaw("Mouse ScrollWheel");
         Vector3 zoom = -transform.up * zoomInput * zoomSpeed * 1000f;
         return zoom;
+    }
+
+    private Vector3 Clamp(Vector3 vector)
+    {
+        vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+        vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+        vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+
+        return vector;
     }
 }
