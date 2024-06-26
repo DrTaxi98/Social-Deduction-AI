096868dd42f03392e03c26999e0053db23d839e7
Andrea Tassi <andreatassi98@gmail.com>
2024-06-26 03:26:05 +0200

    Tasks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region singleton
     public static GameManager Instance { get; private set; }
 
     private void Awake()
@@ -16,23 +17,46 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
     }
+    #endregion singleton
+
+    public int randomSeed = 0;
 
     private List<Agent> agents = new List<Agent>();
+    private POI[] pois = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (randomSeed == 0)
+            randomSeed = (int)System.DateTime.Now.Ticks;
+        Random.InitState(randomSeed);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        pois = FindObjectsByType<POI>(FindObjectsSortMode.InstanceID);
     }
 
     public void AddAgent(Agent agent)
     {
         agents.Add(agent);
+    }
+
+    private POI RandomPOI()
+    {
+        int randomIndex = Random.Range(0, pois.Length);
+        return pois[randomIndex];
+    }
+
+    public HashSet<POI> RandomPOIs(int n)
+    {
+        n = Mathf.Min(n, pois.Length);
+        HashSet<POI> randomPOIs = new HashSet<POI>();
+        for (int i = 0; i < n; i++)
+        {
+            POI randomPOI;
+            do
+            {
+                randomPOI = RandomPOI();
+            } while (!randomPOIs.Add(randomPOI));
+        }
+        return randomPOIs;
     }
 }
